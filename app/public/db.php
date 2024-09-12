29b363f85063948783c720947cc787fe04ca0193
Bill Yang <bill.yang@live.ca>
2024-09-12 15:43:45 -0700

    changed names

--- app/public/db.php
+++ app/public/db.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:dbname=codelab;host=mysql', 'codelab', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+$pdo = new PDO('mysql:dbname=dev;host=mysql', 'dev', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 $query = $pdo->query('SHOW VARIABLES like "version"');
 
